2e6e9abfb03fa2d0e7c82f11448a447b7f28296f
venkatesh-r <venkypsd@gmail.com>
2025-01-14 10:52:15 +0100

    tailwind installation

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}", // Adjust based on your project structure
+    "./public/index.html", // For simple projects
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
