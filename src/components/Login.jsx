2be256a457664d00adc5b1e46ab64490f718a1f5
venkatesh-r <venkypsd@gmail.com>
2025-01-20 17:17:37 +0100

    login page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,5 +1,36 @@
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className="flex justify-center">
+      <div className="card bg-base-100 w-96 shadow-xl">
+        <div className="card-body">
+          <h2 className="card-title">Login</h2>
+          <label className="form-control w-full max-w-xs">
+            <div className="label">
+              <span className="label-text font-bold">E-Mail</span>
+            </div>
+            <input
+              type="text"
+              placeholder="Type here"
+              className="input input-bordered w-full max-w-xs"
+            />
+          </label>
+          <label className="form-control w-full max-w-xs mt-5">
+            <div className="label">
+              <span className="label-text font-bold">Password</span>
+            </div>
+            <input
+              type="password"
+              placeholder="Type here"
+              className="input input-bordered w-full max-w-xs"
+            />
+          </label>
+          <div className="card-actions justify-center mt-10">
+            <button className="btn btn-primary">Login</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
